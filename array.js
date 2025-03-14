e1ece20da1e851b1beb18f70f0bf8c638e872a4c
mdsajib50 <mdajib850@gmail.com>
2025-03-14 12:19:51 +0600

    33-37 task done

--- array.js
+++ array.js
@@ -173,17 +173,18 @@ let strArr = denseArray.toString();
 // console.log(typeof strArr);
 
 const employees = [
-     { id: 1, name: "Alice", departmentId: 1, salary: 5000 },
-     { id: 2, name: "Bob", departmentId: 2, salary: 7000 },
-     { id: 3, name: "Charlie", departmentId: 3, salary: 4500 },
-     { id: 4, name: "Diana", departmentId: 1, salary: 5500 },
-     { id: 5, name: "Edward", departmentId: 2, salary: 8000 },
-     { id: 6, name: "Fiona", departmentId: 4, salary: 6000 },
-     { id: 7, name: "George", departmentId: 3, salary: 5200 },
-     { id: 8, name: "Helen", departmentId: 4, salary: 7200 },
-     { id: 9, name: "Ian", departmentId: 2, salary: 4800 },
-     { id: 10, name: "Jane", departmentId: 1, salary: 5100 },
+     { id: 1, name: "Alice", departmentId: 1, salary: 5000, skills: ["Communication", "Recruitment"] },
+     { id: 2, name: "Bob", departmentId: 2, salary: 7000, skills: ["JavaScript", "React"] },
+     { id: 3, name: "Charlie", departmentId: 3, salary: 4500, skills: ["SEO", "Content Writing"] },
+     { id: 4, name: "Diana", departmentId: 1, salary: 5500, skills: ["Recruitment", "HR Policies"] },
+     { id: 5, name: "Edward", departmentId: 2, salary: 8000, skills: ["Node.js", "Database Management"] },
+     { id: 6, name: "Fiona", departmentId: 4, salary: 6000, skills: ["Negotiation", "Sales Strategies"] },
+     { id: 7, name: "George", departmentId: 3, salary: 5200, skills: ["Marketing Research", "Social Media"] },
+     { id: 8, name: "Helen", departmentId: 4, salary: 7200, skills: ["Customer Relationship", "Product Presentation"] },
+     { id: 9, name: "Ian", departmentId: 2, salary: 4800, skills: ["Python", "Machine Learning"] },
+     { id: 10, name: "Jane", departmentId: 1, salary: 5100, skills: ["Interviewing", "Employee Engagement"] },
 ];
+
    
 const departments = [
      { id: 1, name: "HR" },
@@ -267,6 +268,7 @@ const firstEmp = employees.find(employee => employee.salary === 5100);
 // console.log(firstEmp);
 
 //  T-030: Find the last employee in the "HR" department.
+
 // const findLastHr = employees.map(employee => employee);
 // const lastEmp = departments.findLast(dep => {
 //      dep.id === findLastHr.departmentId && dep.name === "HR"
@@ -293,15 +295,38 @@ const firstMarketingEmployee = employees.find(emp => {
 const allEmp = employees.filter(employee => employee.salary > 4000);
 console.log(allEmp);
 
-//  T-033: Find the last employee in the "HR" department.
+//  T-033: Find the first employee in the "HR" department.
+const firstHrEmployee = employees.find(emp => {
+     const department = departments.find(dep => dep.id === emp.departmentId);
+     return department && department.name === "HR";
+});
+console.log(firstHrEmployee);
 
 //  T-034: Verify if all employees belong to a department listed in the departments array.
+const allEmpBelongToDep = employees.every(emp => {
+     const department = departments.find(dep => dep.id === emp.departmentId);
+     return department !== undefined;
+});
+
+const allEmpBelongToDep2 = employees.every(employee => employee.departmentId && employee.departmentId !== 0)
+console.log(allEmpBelongToDep);
 
 //  T-035: Log each employee's name and department name to the console.
-
+const empNameWitDepName = employees.forEach(employee=>{
+     const depName = departments.find(dep => dep.id === employee.departmentId);
+     
+     
+     console.log(employee.name, depName?.name);
+     
+})
+// console.log(empNameWitDepName);
 //  T-036: Extract all employee skill names into a single array.
+const allSkills = employees.flatMap(employee => employee.skills);
+console.log(allSkills);
 
 //  T-037: Increment each employee's salary by 10%
+const incrementSalary = employees.map(employee => employee.salary * 0.10 + employee.salary);
+console.log(incrementSalary);
 
 //  T-038: Assume each employee can have multiple skills. Create an array of employee skills and flatten them. Example: [{name: "Alice", skills: ["Excel", "Management"]}, ...].
 
